f437855521f50328f633340838e3d6b70c707674
cynthiaawuor <cynthiaawuor2000@gmail.com>
2026-09-04 10:55:20 +0300

    fix: changed the db client connection to use prisma8 version instead of prismaclient

--- tests/schema.test.ts
+++ tests/schema.test.ts
@@ -1,26 +1,23 @@
-import {it, expect, beforeAll, afterAll} from "vitest";
-import { PrismaClient } from "@prisma/client";
+import { it, expect, beforeAll, afterAll } from "vitest";
+import { db as prisma } from "../src/prisma/db";
 
+beforeAll(async () => {
+  await prisma.connect();
+});
+afterAll(async () => {
+  await prisma.close();
+});
+it("should have database connection", async () => {
+  await expect(prisma.connect()).resolves.not.toThrow();
+});
 
-let prisma: PrismaClient;
- beforeAll(() => {
-    prisma = new PrismaClient()
-
- })
- afterAll( async() => {
-    await prisma.$disconnect()
- } )
-it("should have database connection", async() => {
-    await expect(prisma.$connect()).resolves.not.toThrow()
-})
-
-it("should have users table schema with correct fields", async() => {
-    const user = await prisma.user.findFirst();
-    expect(user).toHaveProperty("user_id")
-    expect(user).toHaveProperty("email");
-    expect(user).toHaveProperty("full_name");
-    expect(user).toHaveProperty("password");
-    expect(user).toHaveProperty("phone_number");
-    expect(user).toHaveProperty("kyc_status");
-    expect(user).toHaveProperty("created_at");
-})
+it("should have users table schema with correct fields", async () => {
+  const user = await prisma.user.findfirst();
+  expect(user).toHaveProperty("user_id");
+  expect(user).toHaveProperty("email");
+  expect(user).toHaveProperty("full_name");
+  expect(user).toHaveProperty("password");
+  expect(user).toHaveProperty("phone_number");
+  expect(user).toHaveProperty("kyc_status");
+  expect(user).toHaveProperty("created_at");
+});
