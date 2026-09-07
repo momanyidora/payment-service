5ffbb3493f5855b74442ba3092450ddbcb24224e
cynthiaawuor <cynthiaawuor2000@gmail.com>
2026-09-07 17:00:30 +0300

    implemeted the SRP between the controller and the schema

--- tests/schema.test.ts
+++ tests/schema.test.ts
@@ -1,56 +1,6 @@
 import "dotenv/config";
 import { it, expect, vi, describe } from "vitest";
 import { db as prisma } from "../src/prisma/db";
-import type { Request, Response } from "express";
-import { createUser } from "../src/controllers/usersController";
-import { randomUUID } from "crypto";
-
-//Mocking response from database
-function mockDatabaseResponse() {
-  const res = {} as Response;
-  res.status = vi.fn().mockReturnValue(res);
-  res.json = vi.fn().mockReturnValue(res);
-  return res;
-}
-describe("createUser tests", () => {
-  const userId = randomUUID();
-  const req = {
-    body: {
-      user_id: userId,
-      email: "ogutu@gmail.com",
-      name: "Ogutu Kwach",
-      password: "password",
-      phone_number: "0712345678",
-      kyc_status: "pending",
-      created_at: new Date().toISOString(),
-    },
-  } as Request;
-  const res = mockDatabaseResponse();
-
-  it("should create a user in the database", async () => {
-    await createUser(req, res);
-    expect(res.status).toHaveBeenCalledWith(201);
-    expect(res.json).toHaveBeenCalledWith(
-      expect.objectContaining({
-        status: "success",
-        data: expect.objectContaining(req.body),
-      }),
-    );
-  });
-
-   it("should fail when creating a user with an existing email", async () => {
-  await prisma.orm.public.User.create(
-      {email: "ogutu@gmail.com"}
-   )
-  
-
-    await expect(createUser(
-        { body: { email: "ogutu@gmail.com" } } as Request,
-        res)
-      ).toThrow("Email already in use")
-    });
-  
-});
 
 it("should have users table schema with correct fields", async () => {
   const user = await prisma.orm.public.User.first();
@@ -64,7 +14,7 @@ it("should have users table schema with correct fields", async () => {
 });
 
 it("should have wallets schema with correct fields", async () => {
-  const wallet = await prisma.orm.public.Wallet.first()
+  const wallet = await prisma.orm.public.Wallet.first();
   expect(wallet).toHaveProperty("wallet_id");
   expect(wallet).toHaveProperty("user_id");
   expect(wallet).toHaveProperty("balance");
